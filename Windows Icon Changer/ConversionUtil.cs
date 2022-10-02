71fd9f91e91dd1c1323e05e6684f4950672d7094
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2022-10-02 14:58:57 +0530

    change package details

--- Windows Icon Changer/ConversionUtil.cs
+++ Windows Icon Changer/ConversionUtil.cs
@@ -1,6 +1,58 @@
-﻿namespace Windows_Icon_Changer;
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using Vanara.PInvoke;
+using WinCopies.GUI.Drawing;
 
-public class ConversionUtil
+namespace Windows.IconChanger;
+
+public static class ConversionUtil
 {
-    
+    public static string PngToIco(string pngFilePath,string icoPath="",int width=256,int height=256)
+    {
+        if (!File.Exists(pngFilePath))
+        {
+            throw new InvalidOperationException(
+                "The png file must exist at provided path, which could not be found");
+        }
+        if (icoPath == "")
+        {
+            icoPath = pngFilePath.Replace("png", "ico", StringComparison.InvariantCultureIgnoreCase);
+        }
+        using var img = new Bitmap(pngFilePath);
+        using var icon = new Bitmap(img, width, height);
+        var bitmapSource = LoadBitmap(icon);
+        var stream = new MemoryStream();
+        BitmapEncoder encoder = new PngBitmapEncoder();
+        encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+        encoder.Save(stream);
+        return Convert(new Bitmap(stream),icoPath);
+    }
+    private static BitmapSource LoadBitmap(Bitmap source)
+    {
+        var ip = source.GetHbitmap();
+        BitmapSource bs;
+
+        try
+        {
+            bs = Imaging.CreateBitmapSourceFromHBitmap(ip, IntPtr.Zero, Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+        }
+        finally
+        {
+            Gdi32.DeleteObject(ip);
+        }
+
+        return bs;
+    }
+
+    private static string Convert(Bitmap bitmap, string icoPath)
+    {
+        var mIcon = new MultiIcon();
+        mIcon.Add("Untitled").CreateFrom(bitmap, IconOutputFormat.Vista);
+        mIcon.SelectedIndex = 0;
+        mIcon.Save(icoPath, MultiIconFormat.ICO);
+        return icoPath;
+    }
 }
--- Windows Icon Changer/IconUtil.cs
+++ Windows Icon Changer/IconUtil.cs
@@ -1,5 +1,100 @@
-﻿namespace Windows_Icon_Changer;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Vanara.PInvoke;
 
-public class Class1
+namespace Windows.IconChanger;
+
+public static class IconUtil
 {
+    /// <summary>
+    /// Set folder icon for a given folder. and instantly refreshes it.
+    /// </summary>
+    /// <param name="icoFile"> full path to the icon file [MUST BE .Ico]</param>
+    /// <param name="folderPath">path to the folder</param>
+    /// <param name="setHiddenAndSystem">if this ico file is to be set hidden and system, default is true, recommended</param>
+    public static void SetFolderIcon(string icoFile, string folderPath,bool setHiddenAndSystem=true)
+    {
+        if (!File.Exists(icoFile))
+        {
+            throw new ArgumentException($"Icon File does not exist at {icoFile}");
+        }
+
+        if (!Directory.Exists(folderPath))
+        {
+            throw new ArgumentException($"Folder Path is not valid: {folderPath}");
+        }
+
+        var newIcoPath = Path.Combine(folderPath, Path.GetFileName(icoFile));
+        if (!File.Exists(newIcoPath))
+        {
+            File.Copy(icoFile,newIcoPath);
+        }
+        icoFile = Path.GetFileName(icoFile);
+        var folderSettings = new Shell32.SHFOLDERCUSTOMSETTINGS
+        {
+            dwMask = Shell32.FOLDERCUSTOMSETTINGSMASK.FCSM_ICONFILE,
+            pszIconFile = icoFile,
+            dwSize = (uint)Marshal.SizeOf(typeof(Shell32.SHFOLDERCUSTOMSETTINGS)),
+            cchIconFile = 0
+        };
+        //FolderSettings.iIconIndex = 0;
+        var unused = Shell32.SHGetSetFolderCustomSettings(ref folderSettings, folderPath, 
+            Shell32.FCS.FCS_FORCEWRITE);
+        Shell32.SHChangeNotify(Shell32.SHCNE.SHCNE_UPDATEDIR, Shell32.SHCNF.SHCNF_PATHW, folderPath);
+        if (setHiddenAndSystem)
+        {
+            SetHiddenAndSystem(newIcoPath);
+        }
+    }
+
+    public static void RefreshIconCache()
+    {
+        _ = Kernel32.Wow64DisableWow64FsRedirection(out _);
+        var objProcess = new Process
+        {
+            StartInfo =
+            {
+                FileName = Environment.GetFolderPath(Environment.SpecialFolder.System) +
+                           "\\ie4uinit.exe",
+                Arguments = "-ClearIconCache",
+                WindowStyle = ProcessWindowStyle.Normal
+            }
+        };
+        objProcess.Start();
+        objProcess.WaitForExit();
+        objProcess.Close();
+        Kernel32.Wow64EnableWow64FsRedirection(true);
+    }
+    /// <summary>
+    /// Deletes set folder icon
+    /// </summary>
+    /// <param name="iconFullPath">Full path to icon file. eg- K:\movies\War\1917\1917.ico</param>
+    public static void DeleteIconsFromFolder(string iconFullPath)
+    {
+        if (!File.Exists(iconFullPath))
+        {
+            throw new InvalidOperationException("Icon file must exist");
+        }
+        var folderName = Path.GetDirectoryName(iconFullPath);
+        var iniFile = Path.Combine(folderName ?? throw new InvalidOperationException("Path must exist"), "desktop.ini");
+        File.Delete(iconFullPath);
+        File.Delete(iniFile);
+        Shell32.SHChangeNotify(Shell32.SHCNE.SHCNE_UPDATEDIR, Shell32.SHCNF.SHCNF_PATHW, folderName);
+    }
+
+    private static void SetHiddenAndSystem(string filePath)
+    {
+        // Set file attribute to "Hidden"
+        if ((File.GetAttributes(filePath) & FileAttributes.Hidden) != FileAttributes.Hidden)
+        {
+            File.SetAttributes(filePath, File.GetAttributes(filePath) | FileAttributes.Hidden);
+        }
+    
+        // Set file attribute to "System"
+        if ((File.GetAttributes(filePath) & FileAttributes.System) != FileAttributes.System)
+        {
+            File.SetAttributes(filePath, File.GetAttributes(filePath) | FileAttributes.System);
+        }
+            
+    }
 }
